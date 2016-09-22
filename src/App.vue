de8851a38cbef8240e7a53bdcc9210546636686a
WillSylvan <silvis.silvestrs@gmail.com>
2016-09-22 21:47:31 +0300

    Ifs explained

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
- 
+<input type="text" v-model="username"> <br>
+<input type="text"> <br>
+<input type="submit" @click="submit()">
+
+<div v-if="!username.length">
+You Must Provide Data! Bitch
+</div> 
+
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      username: ""
+    }
+  },
+  methods: {
+      submit() {
+          if(this.username == "suka") {
+          alert("yoo")
+
+          }
+
+      }
+  }
+
+}
 
 </script>
 
