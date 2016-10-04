806f189cb0ae10d5e369430d4f2579370ecd6625
WillSylvan <silvis.silvestrs@gmail.com>
2016-10-04 21:01:00 +0300

    Dinamic Menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <template>
+	<ul>
+		<li v-for="item in menu"
+		v-bind:class="{ active: item.active }">
+			{{ item.title }} 
+		</li>
+	</ul>
 
 </template>
 
 <script>
-export default {}
-
+export default {
+	data () {
+		return {
+			menu: [
+				{
+					title: "Home",
+					active: true
+				},
+				{
+					title: "Team",
+					active: false
+				},
+				{
+					title: "About",
+					active: false
+				},
+				{
+					title: "Contact",
+					active: false
+				},
+				{
+					title: "real cool",
+					active: false
+				},
+			]
+		}
+	}
+}
 </script>
 
+
 <style>
+
+.active{
+	color: red;
+}
+
 </style>
