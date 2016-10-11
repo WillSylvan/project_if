71874c8b47fdc6075f58c9c3f7534a40e4e6bbd2
WillSylvan <silvis.silvestrs@gmail.com>
2016-10-11 21:54:48 +0300

    AuthorizeShit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
+ 	<input v-model="user.name">
+	<input type="password" v-model="user.pass">
 
+	<button @click="authorize(user.name)">LogIn</button>
+	<pre>
+	 {{ user | json }}
+	</pre>
 </template>
-
 
 <script>
 export default {
-	
+ 	data () {
+ 		return {
+   			user: {
+    			name: "",
+     			pass: ""
+   			}
+		}
+ 	},
+ 	methods: {
+ 		authorize (username, password) {
+ 			console.log(username)
+ 			if (
+ 				username == "admin" 
+ 				&& 
+ 				password == "pass123"
+ 			) {
+ 				alert("You are admin")
+ 			}
+
+ 		}
+
+ 	}
 }
-
 </script>
-
 
 <style>
 
 </style>
-
