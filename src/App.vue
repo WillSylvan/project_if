17b7703fa3e5ea79c176c3547ee9312291b19982
WillSylvan <silvis.silvestrs@gmail.com>
2016-10-13 22:07:08 +0300

    authorize

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
+
+	<div v-if="user.isAdmin">
+		Es Esmu Admins
+		<strong>{{user.name}}</strong>
+		<button @click="logOut()">LogOut</button>
+	</div>
+
+	<div v-if="!user.isAdmin">
+		Es Esmu Parasts Lietotajs
  	<input v-model="user.name">
 	<input type="password" v-model="user.pass">
-
 	<button @click="authorize(user.name, user.pass)">LogIn</button>
+	</div>
+
 	<pre>{{ user | json }}</pre>
+
 </template>
 
 <script>
@@ -12,21 +23,23 @@ export default {
  		return {
    			user: {
     			name: "",
-     			pass: ""
+     			pass: "",
+     			isAdmin: false
    			}
 		}
  	},
  	methods: {
- 		authorize (username, password) {
+ 		authorize (username, password) { 
  			console.log(username)
- 			if (
- 				username == "admin" 
- 				&& 
- 				password == "pass123"
- 			) {
- 				alert("You are admin")
+ 			if (username == "ww" && password == "ww") {
+ 				this.user.isAdmin = true
+ 			} else {
+ 				this.user.pass = ""
  			}
 
+ 		},
+ 		logOut () {
+ 			this.user.isAdmin = false
  		}
 
  	}
